c9a2e7a204f610ee488080aa9cc189b67e2d4271
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-05-05 09:43:08 +0500

    style the ql al editor & toolbar of quill

--- Goole_Docs/src/components/TextEditor.jsx
+++ Goole_Docs/src/components/TextEditor.jsx
@@ -1,13 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 
 function TextEditor() {
+    let wraper=useRef()
+
     useEffect(()=>{
-        new Quill ('#quill',{theme:"snow"})
+        const editor=document.createElement('div')
+        wraper.current.append(editor)
+        new Quill(editor, {
+            modules: {
+              toolbar: [
+                [{ header: [1, 2, false] }],
+                ['bold', 'italic', 'underline','link','color','font'],
+                ['image','video', 'code-block','align'],
+              ],
+            },
+            placeholder: 'Compose an epic...',
+            theme: 'snow', // or 'bubble'
+          });
     },[])
+  
   return (
-    <div id='quill'>
+    <div id='quill' className='editor' ref={wraper}>
       
     </div>
   )
